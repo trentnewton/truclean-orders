e8bed550d2a26a97ec942c97277d2ecda3f5d46a
Trent Newton <info@trentnewton.com>
2021-12-10 10:11:13 +1000

    improved clock script

--- index.php
+++ index.php
@@ -303,20 +303,43 @@
             </div>
             <script src="dist/assets/js/app.js"></script>
             <script>
-              function startTime() {
-                const today = new Date();
-                let h = today.getHours();
-                let m = today.getMinutes();
-                let s = today.getSeconds();
-                m = checkTime(m);
-                s = checkTime(s);
-                document.getElementById("clock").innerHTML =  h + ":" + m + ":" + s;
-                setTimeout(startTime, 1000);
+              var clockID;
+              var yourTimeZoneFrom = +10.00; //time zone value where you are at
+              
+              var d = new Date();  
+              //get the timezone offset from local time in minutes
+              var tzDifference = yourTimeZoneFrom * 60 + d.getTimezoneOffset();
+              //convert the offset to milliseconds, add to targetTime, and make a new Date
+              var offset = tzDifference * 60 * 1000;
+              
+              function UpdateClock() {
+                  var tDate = new Date(new Date().getTime()+offset);
+                  var in_hours = tDate.getHours()
+                  var in_minutes=tDate.getMinutes();
+                  var in_seconds= tDate.getSeconds();
+              
+                  if(in_minutes < 10)
+                      in_minutes = "0"+in_minutes;
+                  if(in_seconds<10)   
+                      in_seconds = "0"+in_seconds;
+                  if(in_hours<10) 
+                      in_hours = "0"+in_hours;
+              
+                document.getElementById("clock").innerHTML = "" 
+                                + in_hours + ":" 
+                                + in_minutes + ":" 
+                                + in_seconds;
+              
               }
-
-              function checkTime(i) {
-                if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
-                return i;
+              function StartClock() {
+                clockID = setInterval(UpdateClock, 500);
+              }
+              
+              function KillClock() {
+                clearTimeout(clockID);
+              }
+              window.onload=function() {
+                StartClock();
               }
             </script>
         </body>
